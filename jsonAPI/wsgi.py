037da24dccc4d6fea7e895955a55b9acfa3efaef
prateekchandan <prateekchandan5545@gmail.com>
2015-02-07 12:18:12 -0500

    changed file permission

--- jsonAPI/wsgi.py
+++ jsonAPI/wsgi.py
@@ -8,7 +8,16 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
+#site.addsitedir('/root/Envs/jsonInput/lib/python2.7/site-packages')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jsonAPI.settings")
+
+path = 'root/ihiredev/jsonAPI'
+if path not in sys.path:
+    sys.path.append(path)
+    sys.path.append('/root/ihiredev/jsonAPI/jsonAPI')
+    sys.path.append('/root/ihiredev')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
